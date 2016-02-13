2e7ff5a5f172eadb154a0682852635b54e4409c0
Spottedone <stonedruid@outlook.com>
2016-02-13 11:53:51 +0100

    Massive update, first styling attempt ^^

---
+++ IronCorgi/ViewModel/CreateAccountModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System.Windows;
+
+namespace IronCorgi.ViewModel
+{
+	public class CreateAccountModel
+	{
+		public string UserName { get; set; }
+		public string EmailAddress { get; set; }
+		public string Password { get; set; }
+
+		public RelayCommand<Window> AddAccountCommand
+		{
+			get
+			{
+				return new RelayCommand<Window>(w => { });
+			}
+		}
+
+		public RelayCommand<Window> CancelCommand
+		{
+			get
+			{
+				return new RelayCommand<Window>(w => 
+				{
+					w.Close();
+				});
+			}
+		}
+	}
+}
--- IronCorgi/ViewModel/ViewModelLocator.cs
+++ IronCorgi/ViewModel/ViewModelLocator.cs
@@ -30,19 +30,8 @@ namespace IronCorgi.ViewModel
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
-            ////}
-            ////else
-            ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
-            ////}
-
             SimpleIoc.Default.Register<MainViewModel>();
+			SimpleIoc.Default.Register<CreateAccountModel>();
         }
 
         public MainViewModel Main
@@ -52,8 +41,16 @@ namespace IronCorgi.ViewModel
                 return ServiceLocator.Current.GetInstance<MainViewModel>();
             }
         }
-        
-        public static void Cleanup()
+
+		public CreateAccountModel CreateAccountData
+		{
+			get
+			{
+				return ServiceLocator.Current.GetInstance<CreateAccountModel>();
+			}
+		}
+
+		public static void Cleanup()
         {
             // TODO Clear the ViewModels
         }
